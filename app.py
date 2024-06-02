d294f072b8438c4d422bf737527dad43f6120159
Saura Naderi <robotsaura@sauras-mbp-2.lan>
2024-06-02 15:28:34 -0700

    add flask app.py update

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the trained model
+with open('cq4_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    input_data = pd.DataFrame([data])
+    prediction = model.predict(input_data)[0]
+    return jsonify({'prediction': int(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
